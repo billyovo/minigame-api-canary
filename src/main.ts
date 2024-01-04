f17735d3f7f568f545bd6f00d8bb200f24e80212
billyovo <30436689+billyovo@users.noreply.github.com>
2024-01-04 12:23:40 +0800

    Update API title and description

--- src/main.ts
+++ src/main.ts
@@ -6,15 +6,15 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
   const config = new DocumentBuilder()
-  .setTitle('Cats example')
-  .setDescription('The cats API description')
-  .setVersion('1.0')
-  .addTag('cats')
-  .build();
-  
-const document = SwaggerModule.createDocument(app, config);
-SwaggerModule.setup('api', app, document);
-  
+    .setTitle('Minigame API')
+    .setDescription(
+      'API related to minigames win record and schedule on the minigame website',
+    )
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('specs', app, document);
   await app.listen(process.env.PORT);
 }
 bootstrap();
